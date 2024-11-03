aa251a407c325311e15e738d9c8e7341ddfa042b
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-11-03 20:58:01 +0900

    [FEAT]: Menuコンポーネントを追加

---
+++ src/components/atoms/menu/menu.stories.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { useMenu } from "./menu";
+import { useRef } from "react";
+import { createPortal } from "react-dom";
+
+const meta: Meta = {};
+
+export default meta;
+type Story = StoryObj<typeof useMenu>;
+
+export const Default: Story = {
+	render: () => {
+		const { RenderMenu, setIsOpen } = useMenu();
+		const ref = useRef<HTMLButtonElement>(null);
+
+		return (
+			<>
+				<div>
+					<div style={{ height: "20px" }}>表示位置の基準</div>
+					<button
+						ref={ref}
+						style={{
+							height: "40px",
+							width: "120px",
+							backgroundColor: "#eeeeee",
+						}}
+						onClick={(e) => {
+							e.stopPropagation();
+							setIsOpen(true);
+						}}
+					>
+						メニューを開く
+					</button>
+				</div>
+				{createPortal(
+					<RenderMenu anchorElement={ref.current}>
+						<>
+							<div>選択肢 1</div>
+							<div>選択肢 2</div>
+						</>
+					</RenderMenu>,
+					document.body
+				)}
+			</>
+		);
+	},
+};
---
+++ src/components/atoms/menu/menu.tsx
@@ -0,0 +1,82 @@
+import { ComponentPropsWithRef, FC, useEffect, useRef, useState } from "react";
+import styles from "./menu.module.css";
+
+/**
+ * Render hooks
+ */
+export const useMenu = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const RenderMenu: FC<Omit<MenuProps, "isOpen" | "setIsOpen">> = ({
+		anchorElement = document.body,
+		children,
+	}) => {
+		return (
+			<Menu anchorElement={anchorElement} isOpen={isOpen} setIsOpen={setIsOpen}>
+				{children}
+			</Menu>
+		);
+	};
+
+	return { RenderMenu, setIsOpen };
+};
+
+// region コンポーネント
+/**
+ * メニュー
+ */
+const Menu: FC<MenuProps> = ({
+	anchorElement,
+	isOpen,
+	setIsOpen,
+	children,
+}) => {
+	const ref = useRef<HTMLDivElement>(null);
+
+	//表示位置を調整
+	useEffect(() => {
+		if (!ref.current || !anchorElement) {
+			return;
+		}
+		const offsetY = 4;
+		const { bottom, left, width } = anchorElement.getBoundingClientRect();
+		const { scrollY, scrollX } = window;
+		ref.current.style.top = `${bottom + scrollY + offsetY}px`;
+		ref.current.style.left = `${left + scrollX}px`;
+		ref.current.style.width = `${width}px`;
+	}, [anchorElement]);
+
+	//背景クリックで閉じるようにする
+	const onClickDocument = (e: MouseEvent) => {
+		if (e.target instanceof Node && ref.current?.contains(e.target)) {
+			return;
+		}
+		setIsOpen(false);
+		document.removeEventListener("click", onClickDocument);
+	};
+	useEffect(() => {
+		document.addEventListener("click", onClickDocument);
+	}, []);
+
+	return (
+		<div
+			ref={ref}
+			className={`${styles["container"]} ${isOpen ? styles["open"] : styles["close"]}`}
+		>
+			{children}
+		</div>
+	);
+};
+// endregion
+
+// region 型定義
+/**
+ * props
+ * メニュー
+ */
+export type MenuProps = ComponentPropsWithRef<"div"> & {
+	anchorElement: HTMLElement; //表示位置の調整に使用する要素
+	isOpen: boolean;
+	setIsOpen: (isOpen: boolean) => void;
+};
+// endregion
