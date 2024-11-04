afb0a8b79b546b35d1380946fc35dac0cb7cf903
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-11-04 20:06:41 +0900

    [FEAT]: メインページにチェックボックスを配置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,41 @@ import {
 	useDropdown,
 } from "../components/molecules/dropdown/dropdown";
 import { GetStaticProps } from "next";
+import { useCheckboxes } from "../components/molecules/checkboxes/checkboxes";
+import { fetchPrefectures } from "../lib/fetch_prefectures";
 
 /**
  * メインページ
  */
-export default function MainPage({ chartOptionPropsCollection }) {
+export default function MainPage({
+	chartOptionPropsCollection,
+	prefCheckboxesPropsCollection,
+}) {
 	const { RenderDropdown, value } = useDropdown();
+	const { RenderCheckboxes } = useCheckboxes(
+		prefCheckboxesPropsCollection.length
+	);
 
 	return (
 		<MainTemplate>
 			<div>
-				<RenderDropdown
+				<Rend,erDropdown
 					value={value}
 					optionPropsCollection={chartOptionPropsCollection}
 				/>
 			</div>
 
-			<div>チェックボックス</div>
+			<div
+				style={{
+					display: "flex",
+					alignItems: "center",
+					gap: "8px 16px",
+					flexWrap: "wrap",
+				}}
+			>
+				<RenderCheckboxes propsCollection={prefCheckboxesPropsCollection} />
+			</div>
+
 			<div>グラフ</div>
 		</MainTemplate>
 	);
@@ -35,9 +53,19 @@ export const getStaticProps = (async () => {
 		{ label: "老年人口", value: "4" },
 	];
 
+	//チェックボックスの選択肢
+	const prefectures = await fetchPrefectures();
+	const prefCheckboxesPropsCollection = prefectures.map((p) => {
+		return {
+			label: p.prefName,
+			value: p.prefCode,
+		};
+	});
+
 	return {
 		props: {
 			chartOptionPropsCollection,
+			prefCheckboxesPropsCollection,
 		},
 	};
 }) satisfies GetStaticProps<{
