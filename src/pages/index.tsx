9bbc7f891925f3a89861540fe083ff5d32d521ca
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-11-04 16:13:39 +0900

    [FEAT]: メインページにプルダウンを表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,45 @@
 import { MainTemplate } from "../components/templates/main/main_template";
+import {
+	OptionProps,
+	useDropdown,
+} from "../components/molecules/dropdown/dropdown";
+import { GetStaticProps } from "next";
 
 /**
  * メインページ
  */
-export default function Main() {
+export default function MainPage({ chartOptionPropsCollection }) {
+	const { RenderDropdown, value } = useDropdown();
+
 	return (
 		<MainTemplate>
-			<div>ドロップダウン</div>
+			<div>
+				<RenderDropdown
+					value={value}
+					optionPropsCollection={chartOptionPropsCollection}
+				/>
+			</div>
+
 			<div>チェックボックス</div>
 			<div>グラフ</div>
 		</MainTemplate>
 	);
 }
+
+export const getStaticProps = (async () => {
+	//プルダウンの選択肢
+	const chartOptionPropsCollection = [
+		{ label: "総人口", value: "1" },
+		{ label: "年少人口", value: "2" },
+		{ label: "生産年齢人口", value: "3" },
+		{ label: "老年人口", value: "4" },
+	];
+
+	return {
+		props: {
+			chartOptionPropsCollection,
+		},
+	};
+}) satisfies GetStaticProps<{
+	chartOptionPropsCollection: Array<OptionProps>;
+}>;
