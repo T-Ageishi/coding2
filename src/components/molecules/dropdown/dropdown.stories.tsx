c6864edec09f2ae670dccb159c94743374928eaf
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-11-04 12:47:00 +0900

    [FEAT]: Dropdownコンポーネントを追加

---
+++ src/components/molecules/dropdown/dropdown.stories.tsx
@@ -0,0 +1,42 @@
+import type { StoryObj } from "@storybook/react";
+import { OptionProps, useDropdown } from "./dropdown";
+
+const meta = {};
+export default meta;
+
+type StoryWithHooks = StoryObj<typeof useDropdown>;
+const DropdownWithHooks = () => {
+	const { RenderDropdown, value } = useDropdown();
+
+	const optionPropsCollection: Array<OptionProps> = [
+		{
+			label: "総人口",
+			value: "1",
+		},
+		{
+			label: "年少人口",
+			value: "2",
+		},
+		{
+			label: "生産年齢人口",
+			value: "3",
+		},
+		{
+			label: "老年人口",
+			value: "4",
+		},
+	];
+
+	return (
+		<RenderDropdown
+			optionPropsCollection={optionPropsCollection}
+			value={value}
+		/>
+	);
+};
+
+export const Default: StoryWithHooks = {
+	render: () => {
+		return <DropdownWithHooks />;
+	},
+};
---
+++ src/components/molecules/dropdown/dropdown.tsx
@@ -0,0 +1,114 @@
+import {
+	ComponentPropsWithoutRef,
+	Dispatch,
+	FC,
+	MouseEventHandler,
+	SetStateAction,
+	useRef,
+	useState,
+} from "react";
+import { useMenu } from "../../atoms/menu/menu";
+import { createPortal } from "react-dom";
+import styles from "./dropdown.module.css";
+
+export const useDropdown = () => {
+	const [value, setValue] = useState<string | undefined>(undefined);
+
+	const RenderDropdown: FC<Omit<DropdownProps, "setValue">> = (props) => {
+		return <Dropdown {...props} value={value} setValue={setValue} />;
+	};
+
+	return { RenderDropdown, value };
+};
+
+//region コンポーネント
+const Dropdown: FC<DropdownProps> = ({
+	value = undefined,
+	setValue,
+	optionPropsCollection,
+}) => {
+	const ref = useRef<HTMLDivElement>();
+	const { RenderMenu, isOpen, setIsOpen } = useMenu();
+
+	const onClickDropdown: MouseEventHandler<HTMLDivElement> = (e) => {
+		e.stopPropagation();
+		setIsOpen(true);
+	};
+	const onClickOption: MouseEventHandler<HTMLDivElement> = (e) => {
+		setValue(e.currentTarget.dataset.value);
+	};
+
+	return (
+		<>
+			<div
+				ref={ref}
+				onClick={onClickDropdown}
+				className={`${styles["container"]} ${isOpen ? styles["open"] : ""}`}
+			>
+				<div className={styles["labelWrapper"]}>
+					<span className={styles["label"]}>
+						{optionPropsCollection.find((p) => p.value === value)?.label ??
+							"選択してください"}
+					</span>
+				</div>
+				<div className={styles["iconWrapper"]}>
+					<div className={`${styles["icon"]}`}>▼</div>
+				</div>
+			</div>
+
+			{/* 選択肢部分 */}
+			{createPortal(
+				<RenderMenu anchorElement={ref.current}>
+					{optionPropsCollection.map((p) => (
+						<Option
+							key={p.value}
+							{...p}
+							onClick={onClickOption}
+							className={value === p.value ? styles["active"] : ""}
+						/>
+					))}
+				</RenderMenu>,
+				document.body
+			)}
+		</>
+	);
+};
+
+/**
+ * 選択肢
+ */
+const Option: FC<OptionProps> = ({ label, value, className, onClick }) => {
+	return (
+		<div
+			className={`${styles["optionContainer"]} ${className}`}
+			data-value={value}
+			onClick={onClick}
+		>
+			<div className={styles["optionLabelWrapper"]}>
+				<span className={styles["optionLabel"]}>{label}</span>
+			</div>
+		</div>
+	);
+};
+//endregion
+
+//region 型定義
+/**
+ * props
+ * ドロップダウン
+ */
+export type DropdownProps = ComponentPropsWithoutRef<"div"> & {
+	value: string | undefined;
+	setValue: Dispatch<SetStateAction<string>>;
+	optionPropsCollection: Array<OptionProps>;
+};
+
+/**
+ * props
+ * 選択肢
+ */
+export type OptionProps = ComponentPropsWithoutRef<"div"> & {
+	label: string;
+	value: string;
+};
+//endregion
