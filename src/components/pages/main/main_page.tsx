490acb56cfce030f7e98ea87008dc4fb282df600
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-11-05 23:21:10 +0900

    [FIX]: ビルドと画面描画ができるようにする

--- src/components/pages/main/main_page.tsx
+++ src/components/pages/main/main_page.tsx
@@ -2,6 +2,14 @@ import { PrefCode, Prefecture } from "../../../lib/fetch_prefectures";
 import { PopulationComposition } from "../../../lib/fetch_population_composition";
 import { MainTemplate } from "../../templates/main/main_template";
 import { FC } from "react";
+import { useDropdown } from "../../molecules/dropdown/dropdown";
+import { useCheckboxes } from "../../molecules/checkboxes/checkboxes";
+import {
+	ChartData,
+	LineProps,
+	ResasChart,
+} from "../../molecules/resas_chart/resas_chart";
+import { getChartOptions } from "../../../lib/get_chart_options";
 
 /**
  * メインページ
@@ -11,9 +19,83 @@ export const MainPage: FC<MainPageProps> = ({
 	prefectures,
 	populationCompositionMap,
 }) => {
+	const { RenderDropdown, value } = useDropdown();
+	const { RenderCheckboxes, checkList } = useCheckboxes(prefectures.length);
+
+	const linePropsCollection: Array<LineProps> = [];
+	const chartDataCollection: Array<ChartData> = [];
+	if (value !== undefined && checkList.filter((v) => v).length > 0) {
+		checkList.forEach((isChecked, index) => {
+			if (!isChecked) {
+				return;
+			}
+			const prefCode = String(prefectures[index].prefCode);
+			const prefName = prefectures[index].prefName;
+			const rd = populationCompositionMap[prefCode][Number(value) - 1];
+
+			linePropsCollection.push({ dataKey: prefCode, name: prefName });
+			rd.data.forEach((d) => {
+				const { year, value } = d;
+
+				let ok = false;
+				chartDataCollection.forEach((chartData) => {
+					if (chartData.year != year) {
+						return;
+					}
+					ok = true;
+					chartData[prefCode] = value;
+				});
+
+				if (!ok) {
+					chartDataCollection.push({
+						year: year,
+						[prefCode]: value,
+					});
+				}
+			});
+		});
+	}
+
 	return (
 		<MainTemplate>
-			<div></div>
+			<div style={{ width: "100%" }}>
+				<RenderDropdown
+					value={value}
+					optionPropsCollection={getChartOptions()}
+				/>
+			</div>
+
+			<div
+				style={{
+					width: "100%",
+					display: "flex",
+					alignItems: "center",
+					gap: "8px 16px",
+					flexWrap: "wrap",
+				}}
+			>
+				<RenderCheckboxes
+					propsCollection={prefectures.map((p) => ({
+						label: p.prefName,
+						value: String(p.prefCode),
+					}))}
+				/>
+			</div>
+
+			<div
+				style={{
+					width: "100%",
+					height: "300px",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+			>
+				<ResasChart
+					linePropsCollection={linePropsCollection}
+					chartDataCollection={chartDataCollection}
+				/>
+			</div>
 		</MainTemplate>
 	);
 };
@@ -29,7 +111,6 @@ export type MainPageProps = {
 /**
  * 都道府県ごとの人口構成データマップ
  */
-export type PopulationCompositionMap = Map<
-	PrefCode,
-	Array<PopulationComposition>
->;
+export type PopulationCompositionMap = {
+	[prefCode: PrefCode]: Array<PopulationComposition>;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,96 +1,21 @@
-import { MainTemplate } from "../components/templates/main/main_template";
-import { useDropdown } from "../components/molecules/dropdown/dropdown";
 import { GetStaticProps } from "next";
-import { useCheckboxes } from "../components/molecules/checkboxes/checkboxes";
 import { fetchPrefectures } from "../lib/fetch_prefectures";
 import { fetchPopulationCompositions } from "../lib/fetch_population_composition";
 import {
-	ChartData,
-	LineProps,
-	ResasChart,
-} from "../components/molecules/resas_chart/resas_chart";
-import { getChartOptions } from "../lib/get_chart_options";
-import {
+	MainPage,
 	MainPageProps,
 	PopulationCompositionMap,
 } from "../components/pages/main/main_page";
 
 /**
  * メインページ
- * @@todo リファクタリング
  */
-export default function MainPage({ prefData, populationComposition }) {
-	const { RenderDropdown, value } = useDropdown();
-	const { RenderCheckboxes, checkList } = useCheckboxes(prefData.length);
-
-	const linePropsCollection: Array<LineProps> = [];
-	const chartDataCollection: Array<ChartData> = [];
-	if (value !== undefined && checkList.filter((v) => v).length > 0) {
-		checkList.forEach((isChecked, index) => {
-			if (!isChecked) {
-				return;
-			}
-			const prefCode = String(prefData[index].value);
-			const prefName = prefData[index].label;
-			const rd = populationComposition[prefCode][Number(value) - 1];
-
-			linePropsCollection.push({ dataKey: prefCode, name: prefName });
-			rd.data.forEach((d) => {
-				const { year, value } = d;
-
-				let ok = false;
-				chartDataCollection.forEach((chartData) => {
-					if (chartData.year != year) {
-						return;
-					}
-					ok = true;
-					chartData[prefCode] = value;
-				});
-
-				if (!ok) {
-					chartDataCollection.push({
-						year: year,
-						[prefCode]: value,
-					});
-				}
-			});
-		});
-	}
-
+export default function Main({ prefectures, populationCompositionMap }) {
 	return (
-		<MainTemplate>
-			<div>
-				<RenderDropdown
-					value={value}
-					optionPropsCollection={getChartOptions()}
-				/>
-			</div>
-
-			<div
-				style={{
-					display: "flex",
-					alignItems: "center",
-					gap: "8px 16px",
-					flexWrap: "wrap",
-				}}
-			>
-				<RenderCheckboxes propsCollection={prefData} />
-			</div>
-
-			<div
-				style={{
-					height: "300px",
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-				}}
-			>
-				<ResasChart
-					linePropsCollection={linePropsCollection}
-					chartDataCollection={chartDataCollection}
-				/>
-			</div>
-		</MainTemplate>
+		<MainPage
+			prefectures={prefectures}
+			populationCompositionMap={populationCompositionMap}
+		/>
 	);
 }
 
@@ -99,10 +24,10 @@ export const getStaticProps = (async () => {
 	const prefectures = await fetchPrefectures();
 
 	//人口構成データ
-	const populationCompositionMap: PopulationCompositionMap = new Map();
+	const populationCompositionMap: PopulationCompositionMap = {};
 	for await (const { prefCode } of prefectures) {
-		const populationCompositions = await fetchPopulationCompositions(prefCode);
-		populationCompositionMap.set(prefCode, populationCompositions);
+		populationCompositionMap[prefCode] =
+			await fetchPopulationCompositions(prefCode);
 
 		//1秒あたり5リクエストまでの制限があるため待機
 		new Promise((resolve) => {
