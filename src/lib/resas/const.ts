b9775ee6062cfbe391027aa2f390513e8ee0c9ea
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-11-07 07:54:39 +0900

    [REFACTOR]: グラフの種類を定数にする

---
+++ src/lib/resas/const.ts
@@ -0,0 +1,23 @@
+/**
+ * グラフの種類
+ * 総人口
+ */
+export const CHART_TYPE_ALL = "0";
+
+/**
+ * グラフの種類
+ * 年少人口
+ */
+export const CHART_TYPE_YOUNG = "1";
+
+/**
+ * グラフの種類
+ * 生産年齢人口
+ */
+export const CHART_TYPE_WORKING_AGE = "2";
+
+/**
+ * グラフの種類
+ * 老年人口
+ */
+export const CHART_TYPE_ELDERLY = "3";
