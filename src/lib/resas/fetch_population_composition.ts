fbf0485fbb063ec57e58af841cb74fc78670bf98
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-11-09 10:06:34 +0900

    [FIX]: try-catchで囲む

--- src/lib/resas/fetch_population_composition.ts
+++ src/lib/resas/fetch_population_composition.ts
@@ -1,23 +1,24 @@
 import { PopulationComposition } from "./index.d";
 
 /**
- * 人口構成データを取得
+ * 人口構成データを取得する
  */
 export async function fetchPopulationCompositions(
 	prefCode: string | number
 ): Promise<Array<PopulationComposition>> {
 	try {
-		//@@todo エラーハンドリング
-		const data = await fetch(
+		const response = await fetch(
 			`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}&cityCode=-`,
 			{
 				method: "GET",
 				headers: [["X-API-KEY", process.env.RESAS_KEY ?? ""]],
 			}
 		);
-		const json = await data.json();
+
+		const json = await response.json();
+
 		return json.result.data;
 	} catch (e) {
-		throw new Error(e.message ?? "データ取得中のエラー");
+		console.error(e);
 	}
 }
--- src/lib/resas/fetch_prefectures.ts
+++ src/lib/resas/fetch_prefectures.ts
@@ -4,12 +4,16 @@ import { Prefecture } from "./index.d";
  * 都道府県データを取得する
  */
 export async function fetchPrefectures(): Promise<Array<Prefecture>> {
-	const response = await fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
-		method: "GET",
-		headers: [["X-API-KEY", process.env.RESAS_KEY ?? ""]],
-	});
+	try {
+		const response = await fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+			method: "GET",
+			headers: [["X-API-KEY", process.env.RESAS_KEY ?? ""]],
+		});
 
-	const json = await response.json();
+		const json = await response.json();
 
-	return json.result;
+		return json.result;
+	} catch (e) {
+		console.error(e);
+	}
 }
