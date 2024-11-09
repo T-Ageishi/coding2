88ff81dc958765243285dd289f3cdb177fcfab65
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-11-09 11:08:51 +0900

    [FIX]: フォントを調整

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
 import "../styles/global.css";
+import { Noto_Sans_JP } from "next/font/google";
+
+const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<main className={notoSansJP.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
